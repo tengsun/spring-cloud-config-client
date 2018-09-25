3597c5955fc1423a03b862cedfba1ac22f0eb11b
tengsun <tengsun@cn.ibm.com>
2018-09-25 13:55:22 +0800

    setup initial config client project

---
+++ src/main/java/st/demo/DemoConfigClientController.java
@@ -0,0 +1,18 @@
+package st.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoConfigClientController {
+
+	@Value("${from}")
+	private String from;
+	
+	@RequestMapping("/from")
+	public String from() {
+		return this.from;
+	}
+	
+}
